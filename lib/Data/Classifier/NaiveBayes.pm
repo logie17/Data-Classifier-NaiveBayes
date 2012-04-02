f37074484cb913aca04dd3f834c765852271c8a1
Logan <loganbell@gmail.com>
2012-04-01 23:18:03 -0700

    Re-orged methods to be alpha order.

--- lib/Data/Classifier/NaiveBayes.pm
+++ lib/Data/Classifier/NaiveBayes.pm
@@ -29,71 +29,84 @@ has words => (
 
 sub _build_tokenizer { $_[0]->tokenizer_class->new }
 
-sub cat_count {
+sub _cat_count {
     my ($self, $category) = @_;
     $self->categories->{$category};
 }
 
-sub total_count {
-    my ($self) = @_;
-    return sum values %{$self->categories};
+sub _cat_scores {
+    my ($self, $text) = @_;
+
+    my $probs = {};
+
+    for my $cat (keys %{$self->categories}) {
+        $probs->{$cat} = $self->_text_prop($cat, $text);
+    }
+
+    return sort { $a->[1] <=> $b->[1] } map { [$_, $probs->{$_} ] } keys %{$probs};
 }
 
-sub doc_prob {
+sub _doc_prob {
     my ($self, $text, $cat) = @_;
 
     return reduce { $a * $b } @{$self->tokenizer->words($text, sub{
         my $word = shift;
-        return $self->word_weighted_average($word, $cat);
+        return $self->_word_weighted_average($word, $cat);
     })};
 }
 
-sub word_prob {
-    my ($self, $word, $cat ) = @_;
-    return 0.0 if $self->cat_count($cat) == 0;
-    return sprintf("%.2f", $self->word_count($word, $cat) / $self->cat_count($cat));
+sub _inc_cat {
+    my ($self, $cat) = @_;
+    $self->categories->{$cat} ||= 0;
+    $self->categories->{$cat} += 1;
 }
 
-sub word_count {
+sub _inc_word {
+    my ($self, $word, $cat) = @_;
+    $self->words->{$word} ||= {};
+    $self->words->{$word}->{$cat} ||= 0;
+    $self->words->{$word}->{$cat} += 1;
+}
+
+sub _text_prop {
+    my ($self, $cat, $text) = @_;
+    my $cat_prob = ($self->_cat_count($cat) / $self->_total_count);
+    my $doc_prob = $self->_doc_prob($text, $cat);
+    return $cat_prob * $doc_prob;
+}
+
+sub _total_count {
+    my ($self) = @_;
+    return sum values %{$self->categories};
+}
+
+sub _word_count {
     my ($self, $word, $category) = @_;
     return 0.0 unless $self->words->{$word} && $self->words->{$word}->{$category};
     return sprintf("%.2f", $self->words->{$word}->{$category});
 }
 
-sub word_weighted_average {
+sub _word_prob {
+    my ($self, $word, $cat ) = @_;
+    return 0.0 if $self->_cat_count($cat) == 0;
+    return sprintf("%.2f", $self->_word_count($word, $cat) / $self->_cat_count($cat));
+}
+
+sub _word_weighted_average {
     my ($self, $word, $cat ) = @_;
   
     my $weight = 1.0;
     my $assumed_prob = 0.5;
 
     # calculate current probability
-    my $basic_prob = $self->word_prob($word, $cat);
+    my $basic_prob = $self->_word_prob($word, $cat);
 
     # count the number of times this word has appeared in all
     # categories
-    my $totals = sum map { $self->word_count($word, $_) } keys $self->categories;
+    my $totals = sum map { $self->_word_count($word, $_) } keys $self->categories;
   
     # the final weighted average
     return ($weight * $assumed_prob + $totals * $basic_prob) / ($weight + $totals);
-}
-
-sub cat_scores {
-    my ($self, $text) = @_;
-
-    my $probs = {};
-
-    for my $cat (keys %{$self->categories}) {
-        $probs->{$cat} = $self->text_prop($cat, $text);
-    }
-
-    return sort { $a->[1] <=> $b->[1] } map { [$_, $probs->{$_} ] } keys %{$probs};
-}
-
-sub text_prop {
-    my ($self, $cat, $text) = @_;
-    my $cat_prob = ($self->cat_count($cat) / $self->total_count);
-    my $doc_prob = $self->doc_prob($text, $cat);
-    return $cat_prob * $doc_prob;
 }
 
 sub classify {
@@ -102,7 +115,7 @@ sub classify {
     my $max_prob = 0.0;
     my $best = undef;
 
-    my @scores = $self->cat_scores($text);
+    my @scores = $self->_cat_scores($text);
 
     for my $score ( @scores) {
         my ( $cat, $prob ) = @{$score};
@@ -125,25 +138,12 @@ sub classify {
     return $best;
 }
 
-sub inc_cat {
-    my ($self, $cat) = @_;
-    $self->categories->{$cat} ||= 0;
-    $self->categories->{$cat} += 1;
-}
-
-sub inc_word {
-    my ($self, $word, $cat) = @_;
-    $self->words->{$word} ||= {};
-    $self->words->{$word}->{$cat} ||= 0;
-    $self->words->{$word}->{$cat} += 1;
-}
-
 sub train {
     my ( $self, $cat, $string ) = @_;
     $self->tokenizer->words($string, sub{
-        $self->inc_word(shift, $cat);    
+        $self->_inc_word(shift, $cat);    
     });
-    $self->inc_cat($cat);
+    $self->_inc_cat($cat);
 }
 
 1;
@@ -160,8 +160,9 @@ Data::Classifier::NaiveBayes
 
 =head1 DESCRIPTION
 
-This a Naive Bayes classifer. The code for this project is largely based off
-of the work done by alexandru's stuff-classifier originally written in Ruby.
+This a Naive Bayes classifer. The code for this project is largely and
+shamelessly based off of the work done by alexandru's stuff-classifier 
+originally written in Ruby.
 
     https://github.com/alexandru/stuff-classifier
 
@@ -191,6 +192,15 @@ A key value pair of word counts by categories
 A key value pair of catogory counts.
 
 =head1 METHODS
+
+=head2 classify($phrase)
+
+This will return the highest probable category associated with the phrase.
+
+=head2 train($category, $phrase)
+
+This will perform a word count and associate words with a category to later be
+classified.
 
 =head1 SEE ALSO
 
